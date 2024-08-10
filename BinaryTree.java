160ced63d9097966b22453e47a03d63c41e8d75e
Swapnaroop <swapnaroop2001@gmail.com>
2024-08-10 12:05:41 -0400

    Children Sum Property in Binary Tree | O(N) Approach

--- BinaryTree.java
+++ BinaryTree.java
@@ -420,6 +420,33 @@ public class BinaryTree {
         }
     }
 
+    //Children Sum Property in Binary Tree | O(N) Approach
+    public static void changeTree(Node root){
+        if (root==null) return ;
+        int child=0;
+        if (root.left!=null) {
+            child+=root.left.value;
+        }
+        if (root.right!=null) {
+            child+=root.right.value;
+        }
+        if (child>=root.value) {
+            root.value=child;
+        }else{
+            if (root.left!=null) {
+                root.left.value=root.value;
+            }
+            else if(root.right != null) root.right.value=root.value;
+        }
+        changeTree(root.left);
+        changeTree(root.right);
+
+        int total=0;
+        if (root.left!=null)  total=root.left.value;
+        if (root.right!=null) total=root.right.value;
+        if (root.left!=null || root.right!=null) root.value=total;
+    }
+
 
 
 
